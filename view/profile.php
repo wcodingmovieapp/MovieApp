bad6cfb114e4c88b60bb0628976ea2ce4934fb18
James Coding <jamescoding@Jamess-MacBook-Pro.local>
2019-11-14 17:25:13 +0900

    landing page wireframe

---
+++ view/profile.php
@@ -0,0 +1,53 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Profile Wireframe</title>
+</head>
+<body {
+    font-family: Arial;
+    margin: 0;
+}
+
+<form align="right" name="logout" method="post" action="log_out.php" style="black">
+<label class="logoutLblPos">
+<input name="submit2" type="submit" id="submit2" value="logout">
+</label>
+</form>
+
+<style>
+/*This is the header, I think? */
+.header {
+    padding: 50px;
+    text-align: center;
+    background-color: black;
+    color: white;
+    font-size: 25px;
+}
+
+/*This is the page content, I think? */
+.content {padding:20px;
+         text-align: center;
+         background-color: grey;
+         }
+</style>
+
+<div class="header">
+<img src= "../Wcoding/frontend/HTML/MyWebSite/Images/download.png" alt="handsome man"/>
+<h1>James</h1>
+<p>at button JamesIsTheGr8est</p>
+</div>
+
+<div class="content">
+<div class="movie" draggable="true"><p> Chungking Express </p></div>
+<div class="movie" draggable="true"><p> Chungking Express </p></div>
+<div class="movie" draggable="true"><p> Chungking Express </p></div>
+<div class="movie" draggable="true"><p> Chungking Express </p></div>
+<div class="movie" draggable="true"><p> Chungking Express </p></div>
+
+</div>
+
+</body>
+</html>

576d88fb8b57682ed29c8b93529bc70544d061ac
Lindsey Lai <26980057+ldrnek@users.noreply.github.com>
2019-11-21 15:00:15 +0900

    update

--- public/js/movieDB.js
+++ public/js/movieDB.js
@@ -1,87 +1,79 @@
-let userQuery = document.getElementById('title');
-
+let userQuery = document.getElementById("title");
 let movieId = "";
 let movieTitle = "";
 let movieReleaseDate = "";
 let movieDirector = "";
 let movieActors = "";
-let moviePoster = "http://image.tmdb.org/t/p/w185/"
+let moviePoster = "http://image.tmdb.org/t/p/w185/";
 
 function fetchData(user_id) {
   let movie = userQuery.value;
-  fetch(`https://api.themoviedb.org/3/search/movie?api_key=a7a2be31391543b1180047cd25bd3045&language=en-US&query=${movie}`)
-  .then(response => {
-    return response.json()
-  })
-  .then(data => {
-    // Work with JSON data here
-    console.log(data);
-    movieId = data.results[0].id;
-    movieTitle = data.results[0].title;
-    movieReleaseDate = data.results[0].release_date;
-    moviePoster += data.results[0].poster_path;
-    searchMovie(user_id);
-
-  })
-  .catch(err => {
-    // Do something for an error here
-  })
- 
-
+  fetch(
+    `https://api.themoviedb.org/3/search/movie?api_key=a7a2be31391543b1180047cd25bd3045&language=en-US&query=${movie}`
+  )
+    .then(response => {
+      return response.json();
+    })
+    .then(data => {
+      // Work with JSON data here
+      console.log(data);
+      movieId = data.results[0].id;
+      movieTitle = data.results[0].title;
+      movieReleaseDate = data.results[0].release_date;
+      moviePoster += data.results[0].poster_path;
+      searchMovie(user_id);
+    })
+    .catch(err => {
+      // Do something for an error here
+    });
 }
 
+function searchMovie(userId) {
+  fetch(
+    `https://api.themoviedb.org/3/movie/${movieId}/credits?api_key=a7a2be31391543b1180047cd25bd3045`
+  )
+    .then(response => {
+      return response.json();
+    })
+    .then(data => {
+      // Work with JSON data here
 
-
-
-  function searchMovie(userId) {
-
-
-  fetch(`https://api.themoviedb.org/3/movie/${movieId}/credits?api_key=a7a2be31391543b1180047cd25bd3045`)
-  .then(response => {
-    return response.json()
-  })
-  .then(data => {
-    // Work with JSON data here
- 
-    for (let i = 0; i < data.crew.length; i++){
+      for (let i = 0; i < data.crew.length; i++) {
         if (data.crew[i].job === "Director") {
-            movieDirector = data.crew[i].name;
+          movieDirector = data.crew[i].name;
         }
-    } 
-    for (let i = 0; i < data.cast.length; i++){
-        movieActors = data.cast[0].name + " " + data.cast[1].name + " " + data.cast[2].name;
-    }
+      }
+      for (let i = 0; i < data.cast.length; i++) {
+        movieActors =
+          data.cast[0].name + " " + data.cast[1].name + " " + data.cast[2].name;
+      }
 
-    let movieData = {
-      "movieId": movieId,
-      "title": movieTitle,
-      "releaseDate": movieReleaseDate,
-      "director": movieDirector,
-      "actors": movieActors,
-      "poster": moviePoster,
-      "action" : "addMovie",
-      "userId" : userId,
-    }
+      let movieData = {
+        movieId: movieId,
+        title: movieTitle,
+        releaseDate: movieReleaseDate,
+        director: movieDirector,
+        actors: movieActors,
+        poster: moviePoster,
+        action: "addMovie",
+        userId: userId
+      };
 
-    var xhr = new XMLHttpRequest();
-    xhr.open('POST', 'index.php');//trying to globalize the action between FB and GMAIL
-    xhr.onreadystatechange = function() { //폴백
-            if (xhr.readyState == 4 && xhr.status == 200) {
-              //todc
-              const listMovies = JSON.parse(xhr.responseText);
-              
-              console.log();
-              // let obj = JSON.parse(xhr.responseText);
-              
-            }
-    }
-    xhr.send(JSON.stringify(movieData));
+      var xhr = new XMLHttpRequest();
+      xhr.open("POST", "index.php"); //trying to globalize the action between FB and GMAIL
+      xhr.onreadystatechange = function() {
+        //폴백
+        if (xhr.readyState == 4 && xhr.status == 200) {
+          //todc
+          const listMovies = JSON.parse(xhr.responseText);
 
-
-  })
-  .catch(err => {
-    // Do something for an error here
-  })
-  }
-
- 
+          console.log();
+          // let obj = JSON.parse(xhr.responseText);
+        }
+      };
+      xhr.send(JSON.stringify(movieData));
+    })
+    .catch(err => {
+      // Do something for an error here
+    });
+}
